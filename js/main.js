8c29d47163a2636238f71f1f07ba741bc87b5983
doyeol <oktui2002@naver.com>
2024-07-15 21:16:02 +0900

    add footer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import ipads from "../data/ipads.js";
+import navigations from "../data/navigations.js";
 
 // 장바구니
 const basketStarterEl = document.querySelector("header .basket-starter");
@@ -139,8 +140,38 @@ ipads.forEach((ipad) => {
     <p class="tagline">${ipad.tagline}</p>
     <p class="price">₩ ${ipad.price.toLocaleString("en-US")} 부터</p>
     <button class="btn">구입하기</button>
-    <a href="${ipad.url}" class="link">더 알아보기</a>
+    <a href="javascript:void(0)" class="link">더 알아보기</a>
   `;
 
   itemsEl.append(itemEl);
 });
+
+// 푸터 내비게이션 맵 랜더링
+const navigationsEl = document.querySelector("footer .navigations");
+navigations.forEach((nav) => {
+  const mapEl = document.createElement("div");
+  mapEl.classList.add("map");
+
+  let mapList = "";
+  nav.maps.forEach((map) => {
+    mapList += /* html */ `<li>
+      <a href="javascript:void(0)">${map.name}</a>
+    </li>`;
+  });
+
+  mapEl.innerHTML = /* html */ `
+    <h3>
+      <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
+    </h3>
+    <ul>
+      ${mapList}
+    </ul>
+  `;
+
+  navigationsEl.append(mapEl);
+});
+
+// 올해 연도를 적용!
+const thisYearEl = document.querySelector(".this-year");
+thisYearEl.textContent = new Date().getFullYear();
